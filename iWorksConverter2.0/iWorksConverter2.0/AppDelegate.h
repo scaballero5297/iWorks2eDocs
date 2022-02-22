4f0e6c0b347bd96584fa5a667895f2efa2f186a4
Sebastian Caballero <scaba035@fiu.edu>
2022-02-22 13:36:02 -0500

    Xcode Initial Files

---
+++ iWorksConverter2.0/iWorksConverter2.0/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iWorksConverter2.0
+//
+//  Created by Sebastian Caballero on 2/22/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
